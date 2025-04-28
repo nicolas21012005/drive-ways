9a3aeb699cc513f9644ee9dae526400f303c8c74
nicolas21012005 <np07qsf@gmail.com>
2025-04-28 02:49:51 -0500

    test-about-page-problem-imgs

--- js/main.js
+++ js/main.js
@@ -4,12 +4,6 @@ const navMenu = document.getElementById('nav-menu');
 toggleBtn.addEventListener('click', () => {
     navMenu.classList.toggle('show');
 });
-// const toggleBtn = document.getElementById('menu-toggle');
-// const navMenu = document.getElementById('nav-menu');
-
-toggleBtn.addEventListener('click', () => {
-    navMenu.classList.toggle('show');
-});
 function modalVideo() {
     let selector = document.getElementById('modal-1');
     selector.classList.toggle('active');
