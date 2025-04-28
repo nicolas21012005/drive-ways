07a4c43bcf73bb58b04c9f6627c2796c5ab9cc2b
nicolas21012005 <np07qsf@gmail.com>
2025-04-28 01:02:15 -0500

    first-commit

---
+++ js/main.js
@@ -0,0 +1,10 @@
+const toggleBtn = document.getElementById('menu-toggle');
+const navMenu = document.getElementById('nav-menu');
+
+toggleBtn.addEventListener('click', () => {
+    navMenu.classList.toggle('show');
+});
+function modalVideo() {
+    let selector = document.getElementById('modal-1');
+    selector.classList.toggle('active');
+}
